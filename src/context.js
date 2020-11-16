ab97c1bab3c33a0de921fcb73902dea7a062f498
alex s <sanatateacom@gmail.com>
2020-11-16 14:08:04 +0200

    created loading and room components

--- src/context.js
+++ src/context.js
@@ -1,11 +1,34 @@
 import React, {Component} from 'react';
+import items from './data';
 
 const RoomContext = React.createContext();
 
 class RoomProvider extends Component {
     state = {
-        greeting: 'hello',
-        name: 'john'
+        rooms: [],
+        sortedRooms: [],
+        featuredRooms: [],
+        loading: true
+    };
+
+    componentDidMount() {
+        let rooms = this.formatData(items);
+        let featuredRooms = rooms.filter(room => room.featured === true);
+
+        this.setState({
+            rooms,
+            featuredRooms,
+            sortedRooms: rooms,
+            loading: false
+        });
+    }
+
+    formatData(items) {
+        return items.map(item => {
+            let id = item.sys.id;
+            let images = item.fields.images.map(image => image.fields.file.url);
+            return {...item.fields, images, id};
+        });
     }
 
     render() {
